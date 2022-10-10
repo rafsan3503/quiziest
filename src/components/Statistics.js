490fa3309f05977ed6d7bc08373de9ea1108eb89
Mahmud <rafsanchowdhuryrudro@gmail.com>
2022-10-11 01:35:43 +0600

    bar chart added

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,20 +1,74 @@
-import React from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import {
+  LineChart,
+    Line,
+    BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 const Statistics = () => {
+    const data = useLoaderData().data;
+    console.log(data)
     return (
-      <div>
-        <LineChart
-          width={600}
-          height={300}
-          data={data}
-          margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
-        >
-          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-          <XAxis dataKey="name" />
-          <YAxis />
-        </LineChart>
+        <div>
+            <h2 className='text-center text-5xl font-bold text-indigo-400'>Statistics With Chart</h2>
+        <div className="lg:flex justify-center min-h-screen items-center">
+          <LineChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis dataKey='total'/>
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="name"
+              stroke="#8884d8"
+              strokeDasharray="5 5"
+            />
+            <Line
+              type="monotone"
+              dataKey="total"
+              stroke="#82ca9d"
+              strokeDasharray="3 4 5 2"
+            />
+          </LineChart>
+
+          <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis dataKey="total" />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="name" fill="#8884d8" />
+            <Bar dataKey="total" fill="#82ca9d" />
+          </BarChart>
+        </div>
       </div>
     );
 };
--- src/routes.js
+++ src/routes.js
@@ -36,6 +36,7 @@ const router = createBrowserRouter([
             },
             {
                 path: 'statistics',
+                loader: async()=>fetch('https://openapi.programming-hero.com/api/quiz'),
                 element: <Statistics/>
             }
         ]
