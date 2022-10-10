bc0a545afe7c94b96b0fd34de8d145558068fe70
Mahmud <rafsanchowdhuryrudro@gmail.com>
2022-10-10 23:29:02 +0600

    eye button fixed

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { EyeIcon } from "@heroicons/react/24/solid";
-import { ToastContainer, toast } from "react-toastify";
+import Swal from "sweetalert2";
+
 
 const Question = ({ question }) => {
-    console.log(question.correctAnswer)
+    console.log(question)
     const showAnswer = answer => {
-        toast.success(answer, { autoClose: 500 })
+        Swal.fire({
+          title: `Correct answer : ${answer}`,
+          showClass: {
+            popup: "animate__animated animate__fadeInDown",
+          },
+          hideClass: {
+            popup: "animate__animated animate__fadeOutUp",
+          },
+        });
     }
     return (
-      <div className="w-96 mx-auto p-4 shadow-lg rounded-lg my-3 relative">
+      <div className="w-9/12 mx-auto p-4 shadow-lg rounded-lg my-3 relative">
         <EyeIcon
           onClick={() => showAnswer(question.correctAnswer)}
-          className="absolute w-4 h-4 right-2"
+          className="absolute w-6 h-6 right-2"
         ></EyeIcon>
-        <h1>Quiz 1</h1>
+            <h1>Question: {question.question}</h1>
         <div>
-          <input type="radio" name="" id="" />
+            <label htmlFor="quiz">
+                    {
+                        question.options.map(option => 
+                           <input type="radio" name="" id="" value='name'/>
+                       ) 
+                    }
+            </label>
         </div>
       </div>
     );
